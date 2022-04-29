0e7f54ad58fa5e0e0de7277be80c6d3bedbd99a2
sandyr <sandyr@ebi.ac.uk>
2022-04-29 17:00:49 +0100

    adds example query

--- src/components/QuerySequence.jsx
+++ src/components/QuerySequence.jsx
@@ -1,10 +1,20 @@
 import TextareaSequence from "textarea-sequence/dist/textarea-sequence";
 import loadWebComponent from "../utils/loadWebComponent";
 import { useEffect, useRef, useState } from "react";
+import exampleQuery from "../utils/exampleQuery";
 
 const errorBadgeStyle = {
   borderColor: "var(--vf-ui-color--red)",
   color: "var(--vf-ui-color--red)",
+};
+
+const workaroundMultipleSequences = (textarea) => {
+  const { multipleSequences, ...otherErrors } = textarea.errors;
+  if (multipleSequences && Object.values(otherErrors).every((e) => !e)) {
+    //  The only "error" is multiple sequences present.
+    //  This isn't actually an error, but a Nightingale bug means the textarea is still bordered red.
+    document.getElementById("sequence-editor").style.border = "1px solid green";
+  }
 };
 
 const QuerySequence = ({ onStageSequence }) => {
@@ -29,14 +39,26 @@ const QuerySequence = ({ onStageSequence }) => {
   }, [textAreaSequenceRef]);
   return (
     <div className="vf-stack vf-stack--400">
-      <textarea-sequence
-        ref={textAreaSequenceRef}
-        height="10em"
-        min-sequence-length="5"
-        single={false}
-        caseSensitive="true"
-        alphabet="ACTGU "
-      />
+      <div className="vf-stack vf-stack--200">
+        <button
+          className="vf-button vf-button--link vf-button--sm"
+          onClick={async () => {
+            await textAreaSequenceRef.current.quill.setText(exampleQuery);
+            await textAreaSequenceRef.current.cleanUp();
+            workaroundMultipleSequences(textAreaSequenceRef.current);
+          }}
+        >
+          Load an example query
+        </button>
+        <textarea-sequence
+          ref={textAreaSequenceRef}
+          height="10em"
+          min-sequence-length="5"
+          single={false}
+          caseSensitive="true"
+          alphabet="ACTGU "
+        />
+      </div>
       <div className="vf-cluster vf-cluster--200">
         <div className="vf-cluster__inner">
           {errors.hasInvalidCharacters && (
@@ -71,18 +93,8 @@ const QuerySequence = ({ onStageSequence }) => {
           className="vf-button vf-button--secondary vf-button--sm"
           onClick={async () => {
             await textAreaSequenceRef.current.cleanUp();
+            workaroundMultipleSequences(textAreaSequenceRef.current);
             onStageSequence(textAreaSequenceRef.current.sequence);
-            const { multipleSequences, ...otherErrors } =
-              textAreaSequenceRef.current.errors;
-            if (
-              multipleSequences &&
-              Object.values(otherErrors).every((e) => !e)
-            ) {
-              //  The only "error" is multiple sequences present.
-              //  This isn't actually an error, but a Nightingale bug means the textarea is still bordered red.
-              document.getElementById("sequence-editor").style.border =
-                "1px solid green";
-            }
           }}
         >
           Check and autofix queries
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState } from "react";
 import api, { baseUrl, pollingInterval } from "../api";
 import Loading from "./Loading";
 import { toast } from "react-toastify";
